19ba4ba1a6bba47026fc3d28d232264685306109
Jessica Shore <shore011@cougars.csusm.edu>
2021-06-01 19:17:29 -0700

    added CSS

--- hangman/game.js
+++ hangman/game.js
@@ -3,15 +3,11 @@
 //generate a random word from the dictionary
 var word = words[Math.floor(Math.random() * items.length)];
 consol.log(word);
-//empty array for guesses
-var guesses = [];
+
+
 //get a guess from the player
-function add_element_to_array()
-{
-    guesses[x] = document.getElementById("text1").value;
-    alert("Element: " + array[x] + " Added at index " + x);
-    x++;
-    document.getElementById("text1").value = "";
-}
+
+
+   
 
 //check if the letter is valid
