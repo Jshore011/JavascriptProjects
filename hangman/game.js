687927ebdac5fc27106b744d15c604e7f3785488
jessica Shore <shore011@cougars.csusm.edu>
2021-06-02 16:22:30 -0700

    button stuff

--- hangman/game.js
+++ hangman/game.js
@@ -1,5 +1,34 @@
 /* class that handles all game play*/
 
+const startGame = document.getElementById('StartGame');
+startGame.addEventListener('click', function(){
+Draw();
+});
+
+const canvas = document.getElementById("hangman");
+const context = canvas.getContext("2d");
+
+/* draw's gallows*/
+Draw = () => {
+    context.beginPath();
+    context.moveTo(175, 225);
+    context.lineTo(5, 225);
+    context.moveTo(40, 225);
+    context.lineTo(25, 5);
+    context.lineTo(100, 5);
+    context.lineTo(100, 25);
+    context.stroke();
+}
+
+
+/*clears the game*/
+clearCanvas = () => {
+    context.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+
+
+}
 //generate a random word from the dictionary
 var word = words[Math.floor(Math.random() * items.length)];
 consol.log(word);
